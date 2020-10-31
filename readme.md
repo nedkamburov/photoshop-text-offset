55971d72bdc3d80aeec2da0b586c43596e44bde7
Nedislav Kamburov <michelangelo@Nedislavs-MacBook-Pro.local>
2020-10-31 22:42:07 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+### Transform a straight line into a 'diagonal' by offsetting every character on a new line
+
+
+#### Demo
+![Demo of the script](adobe-text-offset-demo2.gif)
+
+
+#### How to use
+[How to run Adobe scripts](https://medium.com/photoshop-tips/how-to-run-script-465d45031de6) An excellent article on various methods to do so
+
+##### TL;DR
+1. Open Photoshop open and **select the desired text layer** to be transformed.
+2. Select File > Scripts > Browse and navigate to the ‘Scriptname.jsx’ file.
+
+OR 
+
+2. Change the ‘Scriptname.jsx’ file using “Open with” -> Photoshop. Now you can execute the script by double-clicking on the file.
+
---
+++ textOffset.jsx
@@ -0,0 +1,39 @@
+﻿// Nedislav G. Kamburov
+// https://nedkamburov.com
+// November, 2020
+// Nifty script for Photoshop allowing the user to offset every character of a word to a new line
+//
+// Different ways to run Photoshop scripts
+// https://medium.com/photoshop-tips/how-to-run-script-465d45031de6
+
+var layer = app.activeDocument.activeLayer;
+(function main() {
+    if (layer && layer.kind == 'LayerKind.TEXT') {
+        var text = layer.textItem.contents;
+        text = text.replace(/\s/g, '');
+        if (text.length >= 1) {
+            executeOffset(layer.textItem);
+            layer.name = text;
+        }
+        else {
+            alert('The text layer is empty. Nothing to offset...')
+        }
+
+    } else {
+        alert('You haven\'t selected a Text layer.\nPlease, make a new layer and perform the action again.')
+    }
+
+})();
+
+function executeOffset(layer) {
+    var text = layer.contents;
+    var newText = '';
+    var offset = '';
+
+    for (var i = 0; i < text.length; i++) {
+        newText += offset + text[i] + '\r'
+        offset += ' ';
+    }
+    layer.contents = newText;
+    return layer;
+}
